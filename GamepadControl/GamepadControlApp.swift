e669c860e5a910725226c32608ea40e5604be7f6
Jonathan Lancaster <lancasja@gmail.com>
2024-08-21 20:48:38 -0700

    Implements as MenuBar app with optional window

--- GamepadControl/GamepadControlApp.swift
+++ GamepadControl/GamepadControlApp.swift
@@ -6,13 +6,55 @@
 //
 
 import SwiftUI
-import SwiftData
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
+    @AppStorage("showWindow") var showWindow = false
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.windows.forEach { window in
+            if (window.identifier?.rawValue == "visualizer") {
+                window.delegate = self
+                window.close()
+            }
+        }
+    }
+    
+    func windowWillClose(_ notification: Notification) {
+        print("Closing window")
+        showWindow = false
+    }
+}
 
 @main
 struct GCTestApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    @Environment(\.openWindow) private var openWindow
+    @Environment(\.dismissWindow) private var dismissWindow
+    
+    @AppStorage("showWindow") var showWindow = false
+    
     var body: some Scene {
-        WindowGroup {
+        Window("Visualizer", id: "visualizer") {
             ContentView()
+        }
+        
+        MenuBarExtra("Stellar Gamepad", systemImage: "gamecontroller") {
+            Button(showWindow ? "Close visualizer" : "Open visualizer") {
+                if showWindow {
+                    dismissWindow(id: "visualizer")
+                } else {
+                    openWindow(id: "visualizer")
+                }
+                
+                showWindow.toggle()
+            }
+            
+            Divider()
+            
+            Button("Quit") {
+                NSApplication.shared.terminate(nil)
+            }
         }
     }
 }
